f12077a6568323c66f2c87ab458559bd98e42a84
erike rodrigues <erike.dev@gmail.com>
2022-11-16 00:32:32 -0300

    configurando nodemon/package e yarn

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-let texto : string;
+import express from "express";
 
-texto = "1232132";
 
-console.log(texto);
+const app = express();
+
+const port = 3001;
+app.listen(port, () => {
+	console.log(`server is running on http://localhost:${port}`);
+});
